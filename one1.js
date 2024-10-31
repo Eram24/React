df15e759bb7e7dbed71dcc2febb149cdd36fd3e0
Eram24 <eramfaiz24@gmail.com>
2024-10-31 11:10:32 +0530

    files updates

--- one1.js
+++ one1.js
@@ -10,20 +10,57 @@
 </div> */
 
 import  React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 
-const p=React.createElement("div",
-    {id : "parent"},
-    React.createElement("div",
-        {id: "child"},
-       [React.createElement("h1",{},"hello from react from jamshedpur"),
-        React.createElement("h2",{},"hello from react from jsr"),
-        React.createElement("h1",{},"hello from react from jamshedpur")
-       ]
-    )
+// const p=React.createElement("div",
+//     {id : "parent"},
+//     React.createElement("div",
+//         {id: "child"},
+//        [React.createElement("h1",{},"hello from react from jamshedpur"),
+//         React.createElement("h2",{},"hello from react from jsr"),
+//         React.createElement("h1",{},"hello from react from jamshedpur")
+//        ]
+//     )
+// );
+
+
+//jsx 
+
+//react element
+const p=(<div id="parent">
+           <h1>hello world from jsx </h1>
+</div>);
+
+//react component
+//react functonal component
+
+const Title=()=>(
+<h1 className="title">title from js </h1>
 );
 
+const Heading =()=>(
+    <div className="head">
+    <Title/>      {/* this is called component composition */}
+        <h1>heading from js</h1>
+    </div>
+);
+
+
+
+//react component
+// const Para=()=>{
+// return <h1> 
+// <h2>bye</h2>
+// hello world | Eram Faiz sem3</h1>
+// };
+
+const Para=()=><h1>
+    <h2>hello </h2>
+    Bye</h1>
+
+
+
 const r=ReactDOM.createRoot(document.querySelector("#root"));
-r.render(p);
+r.render(<Para/>);
 
